bcf6c6810b0fbd5029c2227a4893209a775f43cc
TaeGit <tgsong827@gmail.com>
2023-05-08 11:21:37 +0900

    add nft_amount field to bank_account

--- programs/deposit-for-nft/src/instructions/initialize.rs
+++ programs/deposit-for-nft/src/instructions/initialize.rs
@@ -19,6 +19,7 @@ pub fn handle(ctx: Context<Initialize>) -> Result<()> {
     let bank_account = &mut ctx.accounts.bank_account;
     bank_account.deposit_auth = *ctx.accounts.bank_auth.key;
     bank_account.auth_bump = *ctx.bumps.get("pda_auth").unwrap();
+    bank_account.nft_amount = 0;
 
     msg!("Smart Contract is initialized!");
     Ok(())
--- programs/deposit-for-nft/src/states/bank.rs
+++ programs/deposit-for-nft/src/states/bank.rs
@@ -5,8 +5,9 @@ pub struct BankAccount {
     pub deposit_auth: Pubkey,
     pub auth_bump: u8,
     pub sol_vault_bump: Option<u8>,
+    pub nft_amount: u16,
 }
 
 impl BankAccount {
-    pub const LEN: usize = 8 + 32 + 1 + 1 + 1;
+    pub const LEN: usize = 8 + 32 + 1 + 1 + 1 + 2;
 }

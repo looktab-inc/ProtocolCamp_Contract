ec302990e23a9783be56ec45fa4279fcb622401a
TaeGit <tgsong827@gmail.com>
2023-05-08 15:51:26 +0900

    add withdraw instruction

---
+++ programs/deposit-for-nft/src/instructions/withdraw.rs
@@ -0,0 +1,55 @@
+use anchor_lang::{prelude::*, system_program, solana_program::native_token::LAMPORTS_PER_SOL};
+
+use crate::states::BankAccount;
+
+#[derive(Accounts)]
+pub struct WithdrawSolForNft<'info> {
+    #[account(has_one = bank_auth)]
+    pub bank_account: Account<'info, BankAccount>,
+    #[account(seeds = [b"pda-auth", bank_account.key().as_ref()], bump = bank_account.auth_bump)]
+    /// CHECK: no need to check this
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut, seeds = [b"sol-vault", pda_auth.key().as_ref()], bump = bank_account.sol_vault_bump.unwrap())]
+    pub sol_vault: SystemAccount<'info>,
+    #[account(mut)]
+    pub bank_auth: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub client_account: SystemAccount<'info>,
+}
+
+pub fn handle(ctx: Context<WithdrawSolForNft>) -> Result<()> {
+
+    let system_program = &ctx.accounts.system_program;
+    let bank_account = &ctx.accounts.bank_account;
+    let pda_auth = &mut ctx.accounts.pda_auth;
+    let sol_vault = &mut ctx.accounts.sol_vault;
+    let client_account = &mut ctx.accounts.client_account;
+
+    let seeds = &[
+        b"sol_vault",
+        pda_auth.to_account_info().key.as_ref(),
+        &[bank_account.sol_vault_bump.unwrap()],
+    ];
+    let sol_vault_signer = &[&seeds[..]];
+
+    // 1. transfer to client_account 
+    let cpi_accounts_to_client = system_program::Transfer {
+        from: sol_vault.to_account_info(),
+        to: client_account.to_account_info(),
+    };
+    let cpi_to_client = CpiContext::new_with_signer(system_program.to_account_info(), cpi_accounts_to_client, sol_vault_signer);
+    system_program::transfer(cpi_to_client, ((LAMPORTS_PER_SOL as f32) * 0.5) as u64);
+
+
+    // 2. transfer to bank_auth
+    let cpi_accounts_to_bank_auth = system_program::Transfer {
+        from: sol_vault.to_account_info(),
+        to: ctx.accounts.bank_auth.to_account_info(),
+    };
+    let cpi_to_bank_auth = CpiContext::new_with_signer(system_program.to_account_info(), cpi_accounts_to_bank_auth, sol_vault_signer);
+    system_program::transfer(cpi_to_bank_auth, ((LAMPORTS_PER_SOL as f32) * 0.5) as u64);
+
+    
+
+    Ok(())
+}
